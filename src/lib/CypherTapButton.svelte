0383afc32a55faf57fa27019a08ee1c145ec6264
cypherflow <cy@cypherflow.ai>
2025-08-15 13:07:33 -0600

    wip

--- src/lib/CypherTapButton.svelte
+++ src/lib/CypherTapButton.svelte
@@ -3,12 +3,17 @@
 <script lang="ts">
   import "@awesome.me/webawesome/dist/styles/webawesome.css";
   import "@awesome.me/webawesome/dist/components/button/button.js";
+  import "@awesome.me/webawesome/dist/components/popover/popover.js";
   let count: number = $state(0);
   const increment = () => {
     count += 1;
   };
 </script>
 
-<wa-button variant="neutral" onclick={increment}>
-  Cyphertap: updated 0.0.5 {count}
+<wa-button variant="neutral" onclick={increment} id="popover__anchor-button">
+  Cyphertap
 </wa-button>
+
+<wa-popover for="popover__anchor-button" placement="bottom">
+  I'm anchored to a Web Awesome button.
+</wa-popover>
