ead35601f54d2ce338e26e7a510ac3ac8b549b52
cypherflow <cy@cypherflow.ai>
2025-08-15 11:38:47 -0600

    feat: add webawesome components

--- src/lib/CypherTapButton.svelte
+++ src/lib/CypherTapButton.svelte
@@ -1,12 +1,18 @@
 <svelte:options customElement="cyphertap-button" />
 
+<!-- <svelte:head> -->
+<!-- <link rel="stylesheet" href="https://early.webawesome.com/webawesome@3.0.0-beta.4/dist/styles/themes/default.css" /> -->
+<!-- </svelte:head> -->
+
 <script lang="ts">
+  import "@awesome.me/webawesome/dist/styles/webawesome.css";
+  import "@awesome.me/webawesome/dist/components/button/button.js";
   let count: number = $state(0);
   const increment = () => {
     count += 1;
   };
 </script>
 
-<button onclick={increment}>
+<wa-button variant="neutral" onclick={increment}>
   Cyphertap: updated 0.0.5 {count}
-</button>
+</wa-button>
