c7dfec6962747f6483eff2d764385096139ecf95
cypherflow <cy@cypherflow.ai>
2025-08-25 16:46:09 -0600

    added first test button

---
+++ src/lib/components/cyphertap/trigger.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import * as Popover from '$lib/components/ui/popover/index.js';
+	import { Button } from '../ui/button/index.js';
+</script>
+
+<Popover.Root>
+	<Popover.Trigger><Button>Test</Button></Popover.Trigger>
+	<Popover.Content>Place content for the popover here.</Popover.Content>
+</Popover.Root>
